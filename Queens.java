07056d507252567050e250e8a4456e5c8734ec6a
kylejonson <jonsonkyle@gmail.com>
2017-02-20 23:46:36 -0800

    Update Queens.java

--- Queens.java
+++ Queens.java
@@ -3,7 +3,8 @@
  */
 
 public class Queens {
-	
+	//I made it a constructor but I can't remember why I thought it was in the worksheet guide but I guess not
+	//I think I saw another program that did this and I subconsciously copied it
 	public Queens(int size){
 		if(size < 4){
 			System.out.println("No Solutions");
@@ -15,10 +16,12 @@ public class Queens {
 		}
 	}
 	private static int explore(Board b, int row, int total){
+		//Base Case
 		if(row == b.size()+1){
 			b.print();
 			System.out.println();
 			return total + 1;
+		//Recursive Case
 		}else{
 			for(int col = 1; col < b.size()+1; col++){
 				if(b.safe(row, col)){
