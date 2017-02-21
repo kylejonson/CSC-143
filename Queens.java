d77e945540f119586ae10cdaa5b15a7db2044c53
kylejonson <jonsonkyle@gmail.com>
2017-02-20 23:00:14 -0800

    Add files via upload

---
+++ Queens.java
@@ -0,0 +1,36 @@
+/*
+ * @author Kyle Jonson
+ */
+
+public class Queens {
+	
+	public Queens(int size){
+		if(size < 4){
+			System.out.println("No Solutions");
+		}else{
+			Board b = new Board(size);
+			System.out.println("Solutions:");
+			int a = explore(b,1,0);
+			System.out.println("Total Solutions: " + a);
+		}
+	}
+	public static int explore(Board b, int row, int total){
+		if(row == b.size()+1){
+			b.print();
+			System.out.println();
+			return total + 1;
+		}else{
+			for(int col = 1; col < b.size()+1; col++){
+				if(b.safe(row, col)){
+					b.place(row, col);
+					total = explore(b,row+1,total);
+					b.remove(row,col);
+				}	
+			}
+			return total;
+		}
+	}
+	public static void main(String[] args){
+		Queens queen = new Queens(8);
+	}
+}
